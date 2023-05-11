11c57ffd1454adc3c0b2be0749ff21aadc4288fa
sindi702 <hamatajsindi@gmail.com>
2023-05-11 01:12:14 -0700

    monty

--- monty.h
+++ monty.h
@@ -33,16 +33,4 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-int is_num(char *n);
-size_t stack_len(const stack_t *h);
-void build_arr(char *token_array[], char *buffer);
-void find_func(char *token_array[], int y, stack_t **node);
-void swap(stack_t **stack, unsigned int line_number);
-stack_t *push(stack_t **stack, unsigned int line_number, char *n);
-void pall(stack_t **stack, unsigned int __attribute__((unused)) line_number);
-void _free(stack_t *head);
-void add(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-int is_num(char *n);
 #endif
